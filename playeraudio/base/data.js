26a6cc4a712ea6d74b930fbd8b9b6006720e9f62
Kaylazz <64696298+KayLazz@users.noreply.github.com>
2020-07-05 09:29:05 -0300

    Add files via upload

---
+++ playeraudio/base/data.js
@@ -0,0 +1,21 @@
+window.audios = [
+  {
+    title:
+    "Sabaton - Ghost Division",
+    artist: "Sabaton",
+    cover: "sabaton-the-art-of-war.jpeg",
+    file: "ghost-division.mp3"
+  },
+  {
+    title: "Sabaton - Primo Victoria",
+    artist: "Sabaton",
+    cover: "sabaton-primo-victoria",
+    file: "primo-victoria.mp3"
+  },
+  {
+    title: "Sabaton - 82nd All The Way",
+    artist: "Sabaton",
+    cover: "sabaton-the-great-war.jpg",
+    file: "82nd-all-the-way.mp3"
+  }
+];
